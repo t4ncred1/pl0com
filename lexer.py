6f5ec508a701a31372477566ea92cca5badd1c3e
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2017-03-24 00:05:50 +0100

    Detab everything!!

--- lexer.py
+++ lexer.py
@@ -4,56 +4,56 @@ __doc__='''Simple lexer for PL/0 using generators'''
 
 # Tokens can have multiple definitions if needed
 symbols =  { 
-	'lparen' : ['('], 
-	'rparen' : [')'], 
-	'times'  : ['*'], 
-	'slash'  : ['/'], 
-	'plus'   : ['+'], 
-	'minus'  : ['-'], 
-	'eql'    : ['='], 
-	'neq'    : ['!='], 
-	'lss'    : ['<'], 
-	'leq'    : ['<='],   
-	'gtr'    : ['>'], 
-	'geq'    : ['>='], 
-	'callsym': ['call'], 
-	'beginsym'  : ['begin'], 
-	'semicolon' : [';'], 
-	'endsym'    : ['end'], 
-	'ifsym'     : ['if'], 
-	'whilesym'  : ['while'], 
-	'becomes'   : [':='], 
-	'thensym'   : ['then'], 
-	'dosym'     : ['do'], 
-	'constsym'  : ['const'], 
-	'comma'     : [','], 
-	'varsym'    : ['var'], 
-	'procsym'   : ['procedure'], 
-	'period'    : ['.'], 
-	'oddsym'    : ['odd'],
-	'print'			: ['!', 'print'],
+  'lparen' : ['('], 
+  'rparen' : [')'], 
+  'times'  : ['*'], 
+  'slash'  : ['/'], 
+  'plus'   : ['+'], 
+  'minus'  : ['-'], 
+  'eql'    : ['='], 
+  'neq'    : ['!='], 
+  'lss'    : ['<'], 
+  'leq'    : ['<='],   
+  'gtr'    : ['>'], 
+  'geq'    : ['>='], 
+  'callsym': ['call'], 
+  'beginsym'  : ['begin'], 
+  'semicolon' : [';'], 
+  'endsym'    : ['end'], 
+  'ifsym'     : ['if'], 
+  'whilesym'  : ['while'], 
+  'becomes'   : [':='], 
+  'thensym'   : ['then'], 
+  'dosym'     : ['do'], 
+  'constsym'  : ['const'], 
+  'comma'     : [','], 
+  'varsym'    : ['var'], 
+  'procsym'   : ['procedure'], 
+  'period'    : ['.'], 
+  'oddsym'    : ['odd'],
+  'print'     : ['!', 'print'],
 }
 
 def token(word):
-	'''Return corresponding token for a given word'''
-	for s in symbols : 
-		if word in symbols[s] :
-			return s
-	try : # If a terminal is not one of the standard tokens but can be converted to float, then it is a number, otherwise, an identifier
-		float(word)
-		return 'number'
-	except ValueError, e :
-		return 'ident'
+  '''Return corresponding token for a given word'''
+  for s in symbols : 
+    if word in symbols[s] :
+      return s
+  try : # If a terminal is not one of the standard tokens but can be converted to float, then it is a number, otherwise, an identifier
+    float(word)
+    return 'number'
+  except ValueError, e :
+    return 'ident'
 
 def lexer(text) :
-	'''Generator implementation of a lexer'''
-	import re
-	from string import split, strip, lower, join
-	t=re.split('(\W+)',text) # Split at non alphanumeric sequences
-	text=join(t,' ') # Join alphanumeric and non-alphanumeric, with spaces
-	words=[ strip(w) for w in split(lower(text)) ] # Split tokens
-	for word in words :
-		yield token(word), word
+  '''Generator implementation of a lexer'''
+  import re
+  from string import split, strip, lower, join
+  t=re.split('(\W+)',text) # Split at non alphanumeric sequences
+  text=join(t,' ') # Join alphanumeric and non-alphanumeric, with spaces
+  words=[ strip(w) for w in split(lower(text)) ] # Split tokens
+  for word in words :
+    yield token(word), word
 
 
 # Test support
@@ -70,10 +70,10 @@ BEGIN
    BEGIN
       CALL square;
       x := x + 1 ;
-			!squ
+      !squ
    END
 END.'''
 
 if __name__ == '__main__' :
-	for t,w in lexer(__test_program) :
-		print t, w
+  for t,w in lexer(__test_program) :
+    print t, w
--- logger.py
+++ logger.py
@@ -4,9 +4,9 @@ __doc__ = '''Logging function using decorators
 Usage: decorate monitored function with "@logger"'''
 
 def logger(f):
-	def wrapped(*args, **kwargs):
-		print 'start', f
-		res = f(*args, **kwargs)
-		print 'end', f
-		return res
-	return wrapped
+  def wrapped(*args, **kwargs):
+    print 'start', f
+    res = f(*args, **kwargs)
+    print 'end', f
+    return res
+  return wrapped
